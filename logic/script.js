1afddc6ec8601aa39ad1f49e384cb04bd2e41fe6
Single-Phantom <misterminecraftter@gmail.com>
2024-11-05 16:02:53 +0500

    media 1/2

--- logic/script.js
+++ logic/script.js
@@ -30,15 +30,15 @@ const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
+const navLink = document.querySelector(".nav-link");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
-    closeIcon.style.display = "none";
+    closeIcon.style.display = "block !important";
     menuIcon.style.display = "none";
   }
 }
